02dd4b8fd06b4831816f289265c23709dc6fde49
Papipz <129078404+cncrous02@users.noreply.github.com>
2026-06-14 13:03:32 +0700

    Initial project setup

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Medical Image Analyzer Assistant",
+    page_icon="🩺"
+)
+
+st.title("🩺 Medical Image Analyzer Assistant")
+
+st.write("Welcome Doctor!")
